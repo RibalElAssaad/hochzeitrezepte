e8e76f293ad2684503bc4f69da6f9aa4c597636f
Ribal El Assaad <r.elassaad@appmea.com>
2021-08-13 19:50:32 +0200

    Fixed bg

---
+++ script.js
@@ -0,0 +1,14 @@
+const onClickHandler = (e) => {
+  const recipeDivs = document.getElementsByClassName("recipe")
+  switch (e) {
+    case "1":
+      console.log("Deutsch")
+      break
+    case "2":
+      console.log("English")
+      break
+    case "3":
+      console.log("Arabic")
+      break
+  }
+}
